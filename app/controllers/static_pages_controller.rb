95d016d1300a5c4b1cc7c9203283f254452655fd
paul <p_ghorra@hotmail.com>
2013-04-26 07:15:04 +0200

    j'ai fait de la merde

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
     if signed_in?
           @micropost  = current_user.microposts.build
           @feed_items = current_user.feed.paginate(page: params[:page])
+          @debate = current_user.debates.build
     end
   end
 
