db9db551100639930de85b80d3e595a45ac931d6
paul <p_ghorra@hotmail.com>
2013-06-21 13:53:26 +0200

    commit

--- app/models/intervention.rb
+++
@@ -1,8 +0,0 @@
-class Intervention < ActiveRecord::Base
-  attr_accessible :content, :title, :performance_id
-  belongs_to :performance
-  
-  validates :performance_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
-
-end
--- app/models/reaction.rb
+++
@@ -1,6 +0,0 @@
-class Reaction < ActiveRecord::Base
-  attr_accessible :argument_id, :content, :user_id
-  
-  belongs_to :user
-  belongs_to :argument
-end
