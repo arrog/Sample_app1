fa60daab6a2b8aff45130caab9524051e9049fd2
paul <p_ghorra@hotmail.com>
2013-07-02 21:20:55 +0200

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ActiveRecord::Base
-    include PublicActivity::Common
     
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
