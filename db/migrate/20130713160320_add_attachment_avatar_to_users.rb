82b3b0b5b6b2af4a77bf6b057e194e3dec442051
paul <p_ghorra@hotmail.com>
2013-07-12 00:41:13 +0200

    ok

---
+++ db/migrate/20130713160320_add_attachment_avatar_to_users.rb
@@ -0,0 +1,10 @@
+class AddAttachmentAvatarToUsers < ActiveRecord::Migration
+  def self.up
+    change_table :users do |t|
+      t.has_attached_file :avatar
+    end
+  end
+
+  def self.down
+  end
+end
