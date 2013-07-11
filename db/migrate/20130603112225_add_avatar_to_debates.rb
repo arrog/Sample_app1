d0e3b6d16f41bbcf8eb17cfab92f1457b7d195ca
paul <p_ghorra@hotmail.com>
2013-07-11 18:05:32 +0200

    ok

--- db/migrate/20130603112225_add_avatar_to_debates.rb
+++ db/migrate/20130603112225_add_avatar_to_debates.rb
@@ -1,8 +1,14 @@
 class AddAvatarToDebates < ActiveRecord::Migration
   def self.up
-    add_attachment :debates, :avatar
-    add_attachment :challenges, :avatar
-    add_attachment :doulins, :avatar
+    change_table :debates do |t|
+      t.attachment :avatar
+    end
+    change_table :challenges do |t|
+      t.attachment :avatar
+    end
+    change_table :doulins do |t|
+      t.attachment :avatar
+    end
   end
 
   def self.down
