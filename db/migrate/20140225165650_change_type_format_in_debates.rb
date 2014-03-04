b4cbf63f488c12bd4295ee01682c7636c32d2848
paul <p_ghorra@hotmail.com>
2014-03-04 20:53:04 +0100

    ok

--- db/migrate/20140225165650_change_type_format_in_debates.rb
+++
@@ -1,9 +0,0 @@
-class ChangeTypeFormatInDebates < ActiveRecord::Migration
-  def up
-    change_column :debates, :type_of_debate, :integer
-   end
-
-   def down
-    change_column :debates, :type_of_debate, :string
-   end
-end
