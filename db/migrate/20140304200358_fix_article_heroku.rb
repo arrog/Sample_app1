0e41603351e6c31c9db14dedc9ed798a6b921a29
paul <p_ghorra@hotmail.com>
2014-03-04 21:09:26 +0100

    ok

--- db/migrate/20140304200358_fix_article_heroku.rb
+++ db/migrate/20140304200358_fix_article_heroku.rb
@@ -2,6 +2,6 @@ class FixArticleHeroku < ActiveRecord::Migration
   def change
     rename_column :debates, :article_id, :article_id_string
     add_column :debates, :article_id, :integer
-    remove_column :contacts, :article_id_string
+    remove_column :debates, :article_id_string
   end
 end
