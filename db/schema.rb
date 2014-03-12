5d3bf8b1a351f54078cdfbe6f9a0de6fd38db217
paul <p_ghorra@hotmail.com>
2014-03-12 20:00:02 +0100

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140307101323) do
+ActiveRecord::Schema.define(:version => 20140312185057) do
 
   create_table "activities", :force => true do |t|
     t.integer  "trackable_id"
