3ec5735bfca638f98462a31febecf5d227027aec
paul <p_ghorra@hotmail.com>
2014-03-23 13:40:53 +0100

    ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,6 @@ class ApplicationController < ActionController::Base
 
   # Force signout to prevent CSRF attacks
   
-  unless Rails.application.config.consider_all_requests_local
-      rescue_from Exception, with: lambda { |exception| render_error 500, exception }
-      rescue_from ActionController::RoutingError, ActionController::UnknownController, ::AbstractController::ActionNotFound, ActiveRecord::RecordNotFound, with: lambda { |exception| render_error 404, exception }
-    end
-
-  
   def handle_unverified_request
     sign_out
     super
