0b08fc2b492d0cedc73ee0b60993dfefc11039b5
paul <p_ghorra@hotmail.com>
2014-03-29 12:53:43 +0100

    ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,17 +18,5 @@ class ApplicationController < ActionController::Base
     session[:previous_url] || root_path
   end
   
-      rescue_from Exception, :with => :render_error
-      rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
-      rescue_from ActionController::RoutingError, :with => :render_not_found
-      rescue_from ActionController::UnknownController, :with => :render_not_found
-      rescue_from ActionController::UnknownAction, :with => :render_not_found
-      
-      def render_not_found(exception)
-        render :template => "/errors/404.html.erb", :status => 404
-      end
 
-      def render_error(exception)
-        render :template => "/errors/500.html.erb", :status => 500
-      end
 end
